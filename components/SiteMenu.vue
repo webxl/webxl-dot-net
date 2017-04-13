0231f7db7c5ac4dc61c9d32d5ba4963cf7c08bef
Matt Motherway <mm@webxl.net>
2017-04-13 12:16:44 -0700

    Initial commit of NUXT.js based webxl.net

---
+++ components/SiteMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="site-menu">
+    <div class="links">
+      <nuxt-link to="/" class="button--gray" v-if="!isHome">Home</nuxt-link>
+      <nuxt-link to="/resume" class="button--gray">Résumé</nuxt-link>
+      <nuxt-link to="/about" class="button--gray">About</nuxt-link>
+
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+  a {
+    margin: 15px 0;
+  }
+</style>
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    computed: {
+      isHome: function () {
+        return this.$route.path === '/';
+      }
+    }
+  }
+</script>
