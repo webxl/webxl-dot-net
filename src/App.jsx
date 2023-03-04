ebca7a2c8f20de6992ff7dece0fbdf4f52e93459
Matt Motherway <mm@webxl.net>
2023-03-04 10:58:09 -0800

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 import styled from "styled-components";
-import bgImage from './assets/blurry-gradient-haikei.svg';
-import flareBgImage from './assets/layered-waves-haikei.svg';
+import bgImage from "./assets/blurry-gradient-haikei.svg";
+import flareBgImage from "./assets/layered-waves-haikei.svg";
 
-import { Suspense } from 'react'
-import {
-  useRoutes,
-} from 'react-router-dom'
+import { Suspense } from "react";
+import { useRoutes } from "react-router-dom";
 
-import routes from '~react-pages'
-import {colors} from "./style-vars.js";
+import routes from "~react-pages";
+import { colors } from "./style-vars.js";
 import SiteMenu from "./components/SiteMenu.jsx";
 import Flare from "./components/Flare.jsx";
 
 const BackgroundWrapper = styled.div`
-
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
@@ -25,33 +21,26 @@ const BackgroundWrapper = styled.div`
   height: 100vh;
   overflow: hidden;
 
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
 
   background-image: url(${bgImage});
   background-size: cover;
-  
-  .flare-wrapper {
+
+  .fg-wrapper {
     background-image: url(${flareBgImage});
     background-size: cover;
     height: 100vh;
   }
-  
+
   footer {
     flex: none;
   }
 
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: ${colors.headingColor};
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
   .content {
-    background: rgba(255, 255, 255, .8);
+    background: rgba(255, 255, 255, 0.8);
     border-radius: 4px;
-    overflow: scroll;
+    overflow: auto;
     max-height: 60vh;
   }
 
@@ -65,14 +54,15 @@ const BackgroundWrapper = styled.div`
   }
 
   ul {
-    line-height: 1.0;
+    line-height: 1;
   }
 
   li {
     margin-left: 2px;
   }
-  
-  a, a:visited {
+
+  a,
+  a:visited {
     font-weight: 400;
     color: ${colors.primaryColor};
   }
@@ -80,19 +70,16 @@ const BackgroundWrapper = styled.div`
 
 const App = () => {
   return (
-      <Suspense fallback={<p>Loading...</p>}>
-        <BackgroundWrapper className="flex flex-col h-screen justify-between">
-            <div className="flare-wrapper flex flex-col h-screen justify-between">
+    <Suspense fallback={<p>Loading...</p>}>
+      <BackgroundWrapper className="flex flex-col h-screen justify-between">
+        <div className="fg-wrapper flex flex-col h-screen justify-between">
           {useRoutes(routes)}
-            <SiteMenu />
-            <Flare />
-            </div>
-        </BackgroundWrapper>
-      </Suspense>
+          <SiteMenu />
+          <Flare />
+        </div>
+      </BackgroundWrapper>
+    </Suspense>
+  );
+};
 
-  )
-}
-
-
-
-export default App
+export default App;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,22 @@
 import Name from "../components/Name.jsx";
+import styled from "styled-components";
+import { colors } from "../style-vars";
+
+const Subtitle = styled.div`
+  font-weight: 300;
+  font-size: 3rem;
+  color: ${colors.headingColor};
+  word-spacing: 5px;
+  padding-bottom: 15px;
+
+  @media (max-width: 640px) {
+    font-size: ${(p) => (p.isLanding ? "3rem" : "2rem")};
+  }
+`;
 
 export default () => (
-    <div className="mt-40 text-center ">
-
-        <Name isLanding/>
-        <div className="subtitle">
-            software engineer
-        </div>
-    </div>
-)
+  <div className="mt-40 text-center ">
+    <Name isLanding />
+    <Subtitle>software engineer</Subtitle>
+  </div>
+);
